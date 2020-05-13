1efb70fe8374ddaebd2333cbc7287b79e64df076
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-05-14 03:43:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Terraform module for Firestore (a.k.a. re-branded Datastore) in GCP
+
+- https://cloud.google.com/datastore/docs/firestore-or-datastore#in_native_mode
---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "0.12.24" # see https://releases.hashicorp.com/terraform/
+  experiments      = [variable_validation]
+}
+
+provider "google" {
+  version = ">= 3.13.0" # see https://github.com/terraform-providers/terraform-provider-google/releases
+}
+
+resource "google_project_service" "firestore_api" {
+  service            = "firestore.googleapis.com"
+  disable_on_destroy = false
+}
+
+# WORK IN PROGRESS.
+# Further development will follow.
+# Pending on popular discussion in https://github.com/terraform-providers/terraform-provider-google/issues/3657
