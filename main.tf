223194f54fb58f3af68359b1653324c2c15203eb
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-08-04 19:34:35 +0800

    Create custom role necessary for accessing firestore console

--- main.tf
+++ main.tf
@@ -12,6 +12,19 @@ resource "google_project_service" "firestore_api" {
   disable_on_destroy = false
 }
 
+resource "google_project_iam_custom_role" "firestore_access" {
+  role_id     = "firestoreAccess"
+  title       = "Firestore Access ${var.name_suffix}"
+  description = "Includes permissions required for accessing firestore. See https://cloud.google.com/datastore/docs/quickstart#before-you-begin"
+  permissions = ["appengine.applications.create", "servicemanagement.services.bind"]
+}
+
+resource "google_project_iam_member" "firestore_access_user_groups" {
+  count  = length(var.user_groups)
+  role   = google_project_iam_custom_role.firestore_access.id
+  member = "group:${var.user_groups[count.index]}"
+}
+
 # WORK IN PROGRESS.
 # Further development will follow.
 # Pending on popular discussion in https://github.com/terraform-providers/terraform-provider-google/issues/3657
