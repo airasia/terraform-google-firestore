a1ae4cf060154e075f3863ede2d7ed8ac7472099
Mohamed Fariz Nazeer <faariz@gmail.com>
2022-06-29 13:41:15 +0800

    Add project id to google_project_iam_* as required by GCP provider 4.0.0

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ terraform {
   required_version = ">= 0.13.1" # see https://releases.hashicorp.com/terraform/
 }
 
+data "google_client_config" "google_client" {}
+
 resource "google_project_service" "firestore_api" {
   service            = "firestore.googleapis.com"
   disable_on_destroy = false
@@ -16,12 +18,14 @@ resource "google_project_iam_custom_role" "firestore_access" {
 
 resource "google_project_iam_member" "firestore_access_user_groups" {
   for_each = toset(var.user_groups)
+  project  = data.google_client_config.google_client.project
   role     = google_project_iam_custom_role.firestore_access.id
   member   = "group:${each.value}"
 }
 
 resource "google_project_iam_member" "datastore_owner_user_groups" {
   for_each = toset(var.user_groups)
+  project  = data.google_client_config.google_client.project
   role     = "roles/datastore.owner" # see https://cloud.google.com/datastore/docs/quickstart#before-you-begin
   member   = "group:${each.value}"
 }
